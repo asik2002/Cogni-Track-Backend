8fd139b2f51a6628075551f5527f5527a8a6865f
asik2002 <mohammedasik.akkim@cogniwide.com>
2024-08-23 12:37:35 -0700

    API naming conventions

---
+++ CogniTrack/src/test/java/com/cogniwide/CogniTrack/CogniTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cogniwide.CogniTrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CogniTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
